568284ae8c9a85e9676ede0803d01791ec7abd4f
famima65536 <famima65536@gmail.com>
2021-11-06 15:24:06 +0900

    SearchTargetTrait no longer has currentTarget
    This functionality is moved into StateManager

--- src/famima65536/lwe/entity/utils/SearchTargetTrait.php
+++ src/famima65536/lwe/entity/utils/SearchTargetTrait.php
@@ -2,40 +2,30 @@
 
 namespace famima65536\lwe\entity\utils;
 
+use famima65536\lwe\entity\utils\policy\SearchEntityPolicy;
 use pocketmine\entity\Entity;
-use pocketmine\math\Vector3;
-use pocketmine\player\Player;
 use pocketmine\world\format\Chunk;
 use pocketmine\world\World;
 
-trait TargetSelectorTrait {
-	protected ?Entity $currentTarget = null;
+trait SearchTargetTrait {
+
 	protected int $searchTargetTick = 0;
 	protected int $targetSearchDistance = 30;
 	private SearchEntityPolicy $searchPolicy;
 
 
 	public function target(): ?Entity{
-		if($this->currentTarget !== null and !$this->currentTarget->isClosed()){
-			return $this->currentTarget;
-		}
 		if($this->searchTargetTick > 0){
 			return null;
 		}
-		$this->currentTarget = $this->searchTarget();
-		if($this->currentTarget !== null){
-			$this->onTargetSelect($this->currentTarget);
+		$target =  $this->getNearestEntityMatchPolicy($this->targetSearchDistance, $this->searchPolicy);
+		if($target !== null){
+			$this->onTargetSelect($target);
 		}
-		return $this->currentTarget;
-	}
-
-
-	public function searchTarget(): ?Entity{
-		return $this->getNearestEntityMatchPolicy($this->targetSearchDistance, $this->searchPolicy);
+		return $target;
 	}
 
 	public function getNearestEntityMatchPolicy(float $maxDistance, SearchEntityPolicy $policy) : ?Entity{
-
 		$pos = $this->getPosition();
 		$minX = ((int) floor($pos->x - $maxDistance)) >> Chunk::COORD_BIT_SIZE;
 		$maxX = ((int) floor($pos->x + $maxDistance)) >> Chunk::COORD_BIT_SIZE;
@@ -46,7 +36,6 @@ trait TargetSelectorTrait {
 
 		/**
 		 * @var Entity|null $currentTarget
-		 * @phpstan-var TEntity|null $currentTarget
 		 */
 		$currentTarget = null;
 		$world = $this->getWorld();
@@ -73,7 +62,7 @@ trait TargetSelectorTrait {
 
 	abstract public function getWorld(): World;
 
-	protected function onTargetSelect(?Entity $currentTarget): void{
+	protected function onTargetSelect(?Entity $target): void{
 	}
 
 }
