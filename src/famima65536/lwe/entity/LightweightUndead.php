249b58f15eee3cda13d011bdd776b263a960285d
famima65536 <famima65536@gmail.com>
2021-11-06 16:16:26 +0900

    Now undead rightfully gets on fire.

--- src/famima65536/lwe/entity/LightweightUndead.php
+++ src/famima65536/lwe/entity/LightweightUndead.php
@@ -10,7 +10,7 @@ abstract class LightweightUndead extends LightweightMonster {
 
 	public function onUpdate(int $currentTick): bool{
 		$timeOfDay = $this->getWorld()->getTimeOfDay();
-		if(!$this->isOnFire() and World::TIME_SUNRISE <= $timeOfDay or $timeOfDay <= World::TIME_SUNSET){
+		if(!$this->isOnFire() and (World::TIME_SUNRISE <= $timeOfDay or $timeOfDay <= World::TIME_SUNSET)){
 			$this->setOnFire(3);
 		}
 		return parent::onUpdate($currentTick);
